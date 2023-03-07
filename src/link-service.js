82d4c4c985b0829d3dbb171361c5cec00af509a0
justin.carvalheiro <justin.carvalheiro16@gmail.com>
2023-03-07 19:25:14 +0100

    systeme onglet terminé

--- src/link-service.js
+++ src/link-service.js
@@ -1,20 +1,21 @@
 class LinkService {
     static getNavLinks() {
         // return [LINK_HOME, LINK_TEAMS ,LINK_RESULTS];
-        return [
-            {
-                "name" : "Home",
-                "url" : LINK_HOME
-            },
-            {
-                "name" : "Teams",
-                "url" : LINK_TEAMS
-            },
-            {
-                "name" : "Results",
-                "url" : LINK_RESULTS
-            },
+        // return [
+        //     {
+        //         "name" : "Home",
+        //         "url" : LINK_HOME
+        //     },
+        //     {
+        //         "name" : "Teams",
+        //         "url" : LINK_TEAMS
+        //     },
+        //     {
+        //         "name" : "Results",
+        //         "url" : LINK_RESULTS
+        //     },
 
-        ]
+        // ]
+        return ["Home", "Teams", "Results"];
     }
 }
--- src/view/nav-bar.js
+++ src/view/nav-bar.js
@@ -3,10 +3,24 @@ export default {
         links : Array,
     },
 
+    methods: {
+        changePage : function(event) {
+            let index = this.links.indexOf(event.target.textContent);
+            let pages = document.querySelectorAll('#app div.onglet');
+
+            pages.forEach(page => { page.classList.remove('show'); })
+            pages[index].classList.add('show');
+            
+            let links = document.querySelectorAll('nav li');
+            links.forEach(link => { link.classList.remove('active'); })
+            links[index].classList.add('active');
+        }
+    },
+
     template: `
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" :href="links[0]">
+            <a class="navbar-brand">
                 <img src="#" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
                 Tactical Teams
             </a>
@@ -14,7 +28,7 @@ export default {
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav" v-for="link in links">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" :href="link.url">{{ link.name }}</a>
+                        <a class="nav-link" aria-current="page" @click="changePage">{{ link }}</a>
                     </li>
                 </ul>
             </div>
