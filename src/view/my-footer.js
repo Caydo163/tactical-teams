1cc84caecb8a1d03928ce2d33186423839571235
justin.carvalheiro <justin.carvalheiro16@gmail.com>
2023-03-18 11:46:21 +0100

    composant footer

---
+++ src/view/my-footer.js
@@ -0,0 +1,8 @@
+export default {
+    template: `
+        <footer class="bg-body-tertiary text-center mt-5 p-3">
+            <p class="mt-3"></p>CARVALHEIRO Justin | FORISSIER Evan - WEB 1</p>
+            <p class="text-muted">Copyright @2023</p>
+        </footer>
+    `
+}
